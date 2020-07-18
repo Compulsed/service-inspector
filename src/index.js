e196b19baf99e6c625b61c39d9a069a89fe65cc5
Dale Salter <dalejsalter@outlook.com>
2020-07-18 17:40:36 +1000

    ADDED: Versions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const { program } = require('commander');
 require('./commands/print');
 require('./commands/update');
 
-program.version('0.0.3');
+program.version('0.0.4');
 
 program.parse(process.argv);
