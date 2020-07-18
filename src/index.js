ce36bf6314a89544591797ce9ac4e651004ad868
Dale Salter <dalejsalter@outlook.com>
2020-07-18 17:49:41 +1000

    ADDED: Code for 0.0.6

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ const { program } = require('commander');
 require('./commands/print');
 require('./commands/update');
 
-program.version('0.0.4');
+program.version('0.0.6');
 
 program.parse(process.argv);
