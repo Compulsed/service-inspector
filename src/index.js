65d3c8f70f86c34a1edf24b4e8b3e87dc65352c9
Dale Salter <dalejsalter@outlook.com>
2020-07-18 17:18:28 +1000

    FIXED: Command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+#! /usr/bin/env node
+
 const { program } = require('commander');
 
 require('./commands/print');
 require('./commands/update');
 
-program.version('0.0.1');
+program.version('0.0.3');
 
 program.parse(process.argv);
